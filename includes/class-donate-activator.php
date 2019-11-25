38590d21739b86c8aea52adf38d9e80d0ca36512
doorb02 <bert.doornbusch@gmail.com>
2019-11-25 06:23:26 +0100

    Initial plugin framework created with WORDPRESS PLUGIN BOILERPLATE GENERATOR https://wppb.me/

---
+++ includes/class-donate-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       bertdoornbusch
+ * @since      1.0.0
+ *
+ * @package    Donate
+ * @subpackage Donate/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Donate
+ * @subpackage Donate/includes
+ * @author     Bert Doornbusch <bert.doornbusch@gmail.com>
+ */
+class Donate_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-donate-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       bertdoornbusch
+ * @since      1.0.0
+ *
+ * @package    Donate
+ * @subpackage Donate/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Donate
+ * @subpackage Donate/includes
+ * @author     Bert Doornbusch <bert.doornbusch@gmail.com>
+ */
+class Donate_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
